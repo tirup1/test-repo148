0a9f66df9b6285febf0ec9b2513b9736cf80f753
dev1 <dev1@mail.com>
2023-09-12 17:40:15 +0530

    modf

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
-class hello()
+class hello()   #from dev1
 {
-	public static void main()
+	public static void main()  #from dev1
 	{
-		system .out println("hello");
+		system .out println("hello from dev1");
 	}
 
 }
-hi tirupathirao
+
